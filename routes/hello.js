3001e91d15628b5a89fc5b23fbea05529cfdd9a7
Tyler Holmes <tholmes@axian.com>
2019-02-27 00:45:00 -0800

    Removes 'Tyler' from app copy.

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET hello page. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'Hello World', user: 'Tyler' });
+  res.render('hello', { title: 'Hello World', user: 'FFTHH' });
 });
 
 module.exports = router;
